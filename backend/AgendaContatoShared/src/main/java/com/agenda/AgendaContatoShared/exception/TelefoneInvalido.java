bb877d2996c19b81df94c733b15ef2814469b7f2
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-05-22 18:35:39 -0300

    feat(exception): criar a exceção TelefoneInvalido

---
+++ backend/AgendaContatoShared/src/main/java/com/agenda/AgendaContatoShared/exception/TelefoneInvalido.java
@@ -0,0 +1,7 @@
+package com.agenda.AgendaContatoShared.exception;
+
+public class TelefoneInvalido extends RuntimeException {
+    public TelefoneInvalido(String message) {
+        super(message);
+    }
+}
