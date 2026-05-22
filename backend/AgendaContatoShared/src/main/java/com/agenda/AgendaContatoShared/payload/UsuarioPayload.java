358501d7f79c6183f14a4ff0516743f61e93c7ae
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-05-22 18:52:53 -0300

    feat(payload): criar o payload UsuarioPayload

---
+++ backend/AgendaContatoShared/src/main/java/com/agenda/AgendaContatoShared/payload/UsuarioPayload.java
@@ -0,0 +1,37 @@
+package com.agenda.AgendaContatoShared.payload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.UUID;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UsuarioPayload {
+
+    @JsonProperty("user_id")
+    private UUID id;
+
+    @JsonProperty(value = "username")
+    private String nome;
+
+    @JsonProperty(value = "email")
+    private String email;
+
+    @JsonProperty(value = "telefone")
+    private String telefone;
+
+    @JsonProperty(value = "senha")
+    private String senha;
+
+    @JsonProperty("tipo_acao")
+    private String tipoAcao;
+}
