98f516334954563c4f9f1474e0035fb0c57d68f6
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-05-27 19:47:13 -0300

    feat(model): criar a entidade Usuario

---
+++ backend/AgendaContatoConsumer/src/main/java/com/agenda/AgendaContatoConsumer/model/Usuario.java
@@ -0,0 +1,24 @@
+package com.agenda.AgendaContatoConsumer.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String username;
+    private String email;
+    private String telefone;
+    private String senha;
+
+}
