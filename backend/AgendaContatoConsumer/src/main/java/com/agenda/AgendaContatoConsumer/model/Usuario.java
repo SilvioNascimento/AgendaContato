3064f2750e4e09af0855009cdcb9efc4c2a15aba
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-05-30 15:23:14 -0300

    feat(model): adicionar outros atributos e seus getters, setters e equals e hashCode

--- backend/AgendaContatoConsumer/src/main/java/com/agenda/AgendaContatoConsumer/model/Usuario.java
+++ backend/AgendaContatoConsumer/src/main/java/com/agenda/AgendaContatoConsumer/model/Usuario.java
@@ -10,6 +10,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -35,4 +37,110 @@ public class Usuario {
 
     @Column(name = "cargo")
     @Enumerated(EnumType.STRING)
+    private Cargo cargo;
+
+    @Column(name = "criado_em")
+    private LocalDateTime criadoEm;
+
+    @Column(name = "atualizado_em")
+    private LocalDateTime atualizadoEm;
+
+    public Usuario(
+            UUID id,
+            String username,
+            String email,
+            String telefone,
+            String senha,
+            Cargo cargo,
+            LocalDateTime criadoEm,
+            LocalDateTime atualizadoEm
+    ) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.telefone = telefone;
+        this.senha = senha;
+        this.cargo = cargo;
+        this.criadoEm = criadoEm;
+        this.atualizadoEm = atualizadoEm;
+    }
+
+    public Usuario() {
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Cargo getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(Cargo cargo) {
+        this.cargo = cargo;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public void setAtualizadoEm(LocalDateTime atualizadoEm) {
+        this.atualizadoEm = atualizadoEm;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(id, usuario.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
