80be58184799d26e5016b9935a01ba4c63cc14dc
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-05-30 15:22:29 -0300

    feat(model): adicionar annotations para nomear cada coluna em cada atributo

--- backend/AgendaContatoConsumer/src/main/java/com/agenda/AgendaContatoConsumer/model/Usuario.java
+++ backend/AgendaContatoConsumer/src/main/java/com/agenda/AgendaContatoConsumer/model/Usuario.java
@@ -1,6 +1,10 @@
 package com.agenda.AgendaContatoConsumer.model;
 
+import com.agenda.AgendaContatoShared.enums.Cargo;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,11 +18,21 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
     private UUID id;
 
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "email")
     private String email;
+
+    @Column(name = "telefone")
     private String telefone;
+
+    @Column(name = "senha")
     private String senha;
 
+    @Column(name = "cargo")
+    @Enumerated(EnumType.STRING)
 }
